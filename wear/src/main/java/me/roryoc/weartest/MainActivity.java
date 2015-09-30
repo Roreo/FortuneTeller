593f782d0968537fd174902fe348d0453a350393
Roreo <itsroryyo@gmail.com>
2015-09-30 20:03:42 +0100

    Added fade in animation for textview and made the green background a darker color

--- wear/src/main/java/me/roryoc/weartest/MainActivity.java
+++ wear/src/main/java/me/roryoc/weartest/MainActivity.java
@@ -9,12 +9,15 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AnimationListener {
 
     private TextView mTextView;
     private SensorManager mSensorManager;
@@ -22,6 +25,7 @@ public class MainActivity extends Activity {
     private float mAccelCurrent; // current acceleration including gravity
     private float mAccelLast; // last acceleration including gravity
     private RelativeLayout colorView;
+    private Animation animFadeIn;
 
     private final SensorEventListener mSensorListener = new SensorEventListener() {
 
@@ -54,8 +58,18 @@ public class MainActivity extends Activity {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+
             }
         });
+
+        // load the animation
+        animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+
+        // set animation listener
+        animFadeIn.setAnimationListener(this);
+
+
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
@@ -63,6 +77,26 @@ public class MainActivity extends Activity {
         mAccelCurrent = SensorManager.GRAVITY_EARTH;
         mAccelLast = SensorManager.GRAVITY_EARTH;
 
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Take any action after completing the animation
+
+
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
 
     }
 
@@ -90,63 +124,83 @@ public class MainActivity extends Activity {
 
         if (answer==1) {
             mTextView.setText("It is certain");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==2){
             mTextView.setText("It is decidedly so");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==3){
             mTextView.setText("Without a doubt");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==4){
             mTextView.setText("Yes, definitely");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==5){
             mTextView.setText("You may rely on it");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==6){
             mTextView.setText("As I see it, yes");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==7){
             mTextView.setText("Most likely");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==8){
             mTextView.setText("Outlook good");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==9){
             mTextView.setText("Yes");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255, 0, 200, 0));
         }else if(answer==10){
             mTextView.setText("Signs point to yes");
-            colorView.setBackgroundColor(Color.GREEN);
+            mTextView.startAnimation(animFadeIn);
+            colorView.setBackgroundColor(Color.argb(255,34,139,34));
         }else if(answer==11){
             mTextView.setText("Reply hazy try again");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.BLUE);
         }else if(answer==12){
             mTextView.setText("Ask again later");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.BLUE);
         }else if(answer==13){
             mTextView.setText("Better not tell you now");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.BLUE);
         }else if(answer==14){
             mTextView.setText("Cannot predict now");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.BLUE);
         }else if(answer==15){
             mTextView.setText("Concentrate and ask again");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.BLUE);
         }else if(answer==16){
             mTextView.setText("Don't count on it");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.RED);
         }else if(answer==17){
             mTextView.setText("My reply is no");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.RED);
         }else if(answer==18){
             mTextView.setText("My sources say no");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.RED);
         }else if(answer==19){
             mTextView.setText("Outlook not so good");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.RED);
         }else if(answer==20){
             mTextView.setText("Very doubtful");
+            mTextView.startAnimation(animFadeIn);
             colorView.setBackgroundColor(Color.RED);
 
         }
